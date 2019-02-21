0486e35cc38200142591f869ac2593ee3d2579ce
Riaño Fernandez, Sandra <sandra.riano@tecnalia.com>
2019-02-21 10:53:41 +0100

    Update main.py -> eliminar funciones "basura"

--- src/main.py
+++ src/main.py
@@ -10,60 +10,6 @@ un fichero main
 import fileutils
 import logging
 import plotutils
-import numpy as np
-import pandas as pd
-
-
-##############################
-#### Funciones Auxiliares ####
-###############################
-
-
-APTS = ["APT1","APT2","APT3","APT4","APT5","APT6","APT7","APT8"]
-
-APTS_2_1 = [1,2,3]
-ENER_2_1 = [1]
-OUTD_2_1 = [1]
-BULD_2_1 = "2.1"
-
-APTS_2_3 = [1,2,3,4,5]
-ENER_2_3 = [1]
-OUTD_2_3 = [1]
-BULD_2_3 = "2.3"
-
-
-
-
-'''
-    This function converts the excel dataframe into appropiate numpy arrays
-    :param df: dataframe containing excel data
-    :param X_labels: Labels with the input data we want to work with
-    :param Y_label: Output labels (Normally Cu)
-    :return:
-'''
-def df2numpy_soloX(df, X_labels = []):
-
-    X = np.array(df[X_labels])
-    return X
-
-
-def dct2numpy(dct, X_labels = [],transpose = False):
-    aux = pd.DataFrame(columns=X_labels)
-    is_ini = True
-    for item in dct.keys():
-        if (is_ini == True):
-            aux = dct[item][X_labels]
-            aux = aux.set_index(dct[item].index.hour)
-            aux = aux.transpose()
-            is_ini = False
-        else:
-            aux_tmp = pd.DataFrame(columns=X_labels)
-            aux_tmp = dct[item][X_labels]
-            aux_tmp = aux_tmp.set_index(dct[item].index.hour)
-            aux_tmp = aux_tmp.transpose()
-            aux = aux.append(aux_tmp,ignore_index=True)
-    aux = aux.fillna(method='ffill')
-    return (np.array(aux[aux.columns]))
 
 if __name__ == "__main__":
    
