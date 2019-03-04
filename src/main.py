6b677c66f4bc4b3a84e76dfa281410bcca233af3
miguel.esteras@tecnalia.com <miguel.esteras@tecnalia.com>
2019-03-04 16:54:20 +0100

    correct formatting scr/main

--- src/main.py
+++ src/main.py
@@ -13,17 +13,14 @@ import plotutils
 import packglobals
 
 if __name__ == "__main__":
-   
-   
+
     logger = packglobals.logger
     logger.info('Starting process...' + 'yeahhh...')
-
 
     BULD = BULD_2_3
     APTS = APTS_2_3
     ENER = ENER_2_3
     OUTD = OUTD_2_3
-
 
     fu = fileutils.FileUtils()
     pu = plotutils.PlotUtils()
@@ -33,9 +30,9 @@ if __name__ == "__main__":
 	instanciadas, fileutils y plotutils
 	"""
     df_out = fu.loadOutdoorData(BULD,OUTD)
-    df_ene = fu.loadEnergyData(BULD,ENER) 
+    df_ene = fu.loadEnergyData(BULD,ENER)
     df_ind = fu.loadIndoorData(BULD,APTS)
-   
+
     pu.doPlotDaily(dct_ind, dct_out, dct_ene)
-    
+
     logger.info(".. process ended")
