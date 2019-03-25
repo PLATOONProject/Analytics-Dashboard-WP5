0bcc8c0cc0ba08614cb3ceae9eb831217300c77d
Tellado Laraudogoitia <borja.tellado@tecnalia.com>
2019-03-25 11:46:12 +0100

    Añado a traceback como import para poder volvar el contenido de la pila al saltar una exceptción. También añado pequeños comentarios indicando que en producción solo debería quedar el log rotativo

--- src/packglobals.py
+++ src/packglobals.py
@@ -5,6 +5,7 @@
 Created on 29/08/2017
 @author: root
 '''
+import traceback
 import logging
 import logging.handlers as handlers
 
@@ -15,15 +16,15 @@ logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 logger = logging.getLogger("hpplanner")
 formatter = logging.Formatter(FORMAT)
 
-# Volcado a fichero plano
+# Volcado a fichero plano, cuando vayas a pasar a producción tu aplicación comenta esta sección
 handler = logging.FileHandler('./output/fhp.log')
 handler.setLevel(logging.DEBUG)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# Volcado a fichero rotativo
+# Volcado a fichero rotativo,en producción solo debería estar este handle activo
 handler = handlers.RotatingFileHandler('./output/rot.log',
-                                       maxBytes=50000, backupCount=20)
+                                       maxBytes=5000000, backupCount=10)
 handler.setLevel(logging.DEBUG)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
